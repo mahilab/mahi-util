4ac2e99bdc95d8f06ac8860b432dc512cc44b954
Evan Pezent <epezent@rice.edu>
2020-03-05 20:13:48 -0600

    spsc example

--- examples/ex_spsc.cpp
+++ examples/ex_spsc.cpp
@@ -1,5 +1,7 @@
 #include <Mahi/Util/Templates/SPSCQueue.hpp>
 #include <Mahi/Util/System.hpp>
+#include <Mahi/Util/Print.hpp>
+#include <Mahi/Util/Timing/Clock.hpp>
 #include <thread>
 #include <string>
 #include <iostream>
@@ -10,25 +12,34 @@ using namespace mahi::util;
 // It's a convenient way to send high throughput messages or data between threads without mutexing
 // See: https://github.com/rigtorp/SPSCQueue for more documentation.
 
-SPSCQueue<std::string> g_queue(256);
+SPSCQueue<std::size_t> g_queue1(2048);
 
-void thrd_func() {
+const std::size_t iters = 4096*4096*16;
+
+void consumer() {
+    std::size_t sum = 0;
     while (true) {
-        while (!g_queue.front());
-        auto msg = *g_queue.front();
-        g_queue.pop();
-        std::cout << msg << std::endl;
-        if (msg == "END")
+        while (!g_queue1.front());
+        auto i = *g_queue1.front();
+        sum += i;
+        g_queue1.pop();
+        if (i == iters-1)
             break;
     }
+    print("Sum: {}",sum);
+}
+
+inline void producer() {
+    for (std::size_t i = 0; i < iters; ++i) 
+        g_queue1.push(i);
 }
 
 int main(int argc, char const *argv[])
 {
-    std::thread thrd(thrd_func);
-    for (int i = 0; i < 512; ++i) 
-        g_queue.push(std::to_string(i));
-    g_queue.push("END");
+    std::thread thrd(consumer);
+    Clock clk;
+    producer();
     thrd.join();
+    print("Time: {} ms",clk.get_elapsed_time().as_milliseconds());
     return 0;
 }
